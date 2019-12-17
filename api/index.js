7e190e3285f13df30e85912aadb4576a597e0a78
Daniel Kanangila <daniel.kanangila@gmail.com>
2019-12-16 22:37:31 -0500

    Refactor api code to use mongodb cloud

--- api/index.js
+++ api/index.js
@@ -1,22 +1,41 @@
 import express from 'express';
-import data from '../src/testData';
-import faker from 'faker';
+import { MongoClient } from 'mongodb';
+import assert from 'assert';
+import config  from '../config';
+
+let mdb;
+
+MongoClient.connect(config.mongodbUri, (err, client) => {
+    assert.equal(null, err);
+    mdb = client.db('linked_tutos');
+    //client.close();
+}) ;
+
 
 const router = express.Router();
-const contests = data.contests.reduce((obj, contest) => {
-    obj[contest.id] = contest;
-    return obj;
-}, {});
 
 router.get('/contests', (req, res) => {
-    res.send({ 
-        contests: contests,
-    });
+    const contests = {};
+    mdb.collection('contests').find({})
+        .project({
+            id: 1,
+            categoryName: 1,
+            contestName: 1,
+        })
+        .each((err, contest) => {
+            if (!contest) {
+                res.send({ contests });
+                return;
+            }
+
+            contests[contest.id] = contest;
+        });
 });
 router.get('/contests/:contestId', (req, res) => {
-    const contest = contests[req.params.contestId];
-    contest.description = faker.lorem.paragraphs();
-    res.send(contest);
+    mdb.collection('contests')
+        .findOne({ id: Number(req.params.contestId) })
+        .then(contest => res.send(contest))
+        .catch(err => console.error(err));
 });
 
 export default router;
--- config.js
+++ config.js
@@ -6,9 +6,10 @@ export const logStart = function(message) {
     console.info('*************');
     console.info(message);
     console.info('*************');
-}
+};
 
 export default {
+    mongodbUri: 'mongodb+srv://danielkanangila:castro1987@kkanagila-zw92l.mongodb.net/test?retryWrites=true&w=majority',
     port: env.PORT || 8080,
     host: env.HOST || '0.0.0.0',
     get serverUrl() {
